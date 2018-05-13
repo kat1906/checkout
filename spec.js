54fafb4c84c93e67f351310457e0dba0eacd87b5
Katie Armstrong <armstrongkatie@hotmail.co.uk>
2018-05-13 15:46:02 +0100

    added price structure object

--- spec.js
+++ spec.js
@@ -1,6 +1,25 @@
 const { expect } = require('chai');
 const Checkout = require('./main');
 
+let prices = {
+    A: {
+        price: 50,
+        discountQuantity: 3,
+        discount: -10
+    },
+    B: {
+        price: 35,
+        discountQuantity: 2,
+        discount: -10
+    },
+    C: {
+        price: 25
+    },
+    D: {
+        price: 12
+    }
+}
+
 describe('Checkout', () => {
     it('Makes new object', () => {
         let checkout1 = new Checkout();
