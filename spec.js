d4dd01e6973f92b6ca16fd3d331504c086cc9d3e
Katie Armstrong <armstrongkatie@hotmail.co.uk>
2018-05-13 15:43:19 +0100

    new instance of checkout returns an object

--- spec.js
+++ spec.js
@@ -1,2 +1,9 @@
-const expect = require('chai');
+const { expect } = require('chai');
 const Checkout = require('./main');
+
+describe('Checkout', () => {
+    it('Makes new object', () => {
+        let checkout1 = new Checkout();
+        expect(checkout1).to.be.an('object');
+    });
+});
