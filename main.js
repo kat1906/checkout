81d97f7a01e9d926c804173a5878c74d0035f2bb
Katie Armstrong <armstrongkatie@hotmail.co.uk>
2018-05-13 15:57:39 +0100

    adds single item to basket

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 class Checkout {
-    constructor() {}
+    constructor(prices) {
+        this.prices = prices;
+        this.basket = {};
+        this.subtotal = 0;
+    }
+    addItem(item) {
+        if(this.basket[item]) this.basket[item]++;
+        else this.basket[item] = 1;
+
+        this.subtotal += this.prices[item].price;
+        return this.basket;
+    }
 }
 
 module.exports = Checkout;
+
+// class Checkout {
+//     constructor() {}
+// }
